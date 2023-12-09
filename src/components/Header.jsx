78266fc34a148d5267199d166056db9ca7a645bb
JessJudd <jessjudd25@gmail.com>
2023-12-09 14:49:04 -0800

    Cleaned up for deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = ({handleChangeFaction,faction}) => {
     return (
         <nav>
-            <div className="gradient-bar"></div>
+            {/* <div className="gradient-bar"></div> */}
             <div className="nav-inner">
                 <h1 className="heading">{faction == 'alliance' ? 'Sindaheri' : 'Saraneth' }'s Salacious Satchels</h1>
                 <div className="button-wrapper">
